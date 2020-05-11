b842c8e605fded6fd795fe3616e380e868d9d400
Dajuan Mcdonald <16410995+FIOpwK@users.noreply.github.com>
2020-05-10 20:30:50 -0400

    working on api-router

--- api/api-router.js
+++ api/api-router.js
@@ -4,19 +4,45 @@ const router = express.Router();
 const User = require('../users/user-model');
 
 
-
-
-
 router.get('/', (req, res) => {
     res.send('<h1>Restricted Page</h1>')
-})
+});
 
 router.get('/users', (req, res) => {
     User.find()
         .then(user => {
             res.status(200).json(user)
-        }).catch(err => res.status(500).json({message: 'Server Error', err}))
-})
+        }).catch(err => res.status(500).json({message: 'Server Error', err}
+        ))
+});
+
+router.post('/register', (req, res) => {
+    const user = req.body;
+
+    const hash = bcrypt.hashSync(user.password, 24)
+    user.password = hash;
+
+    User.add(user)
+        .then(addUser => {
+            res.status(201).json(addUser)
+        }).catch(err => res.status(500).json({message: 'Server Error', err}
+        ))
+});
+
+router.post('/login', (req, res) => {
+    const {username, password} = req.body;
+
+    User.findById({username})
+        .first()
+        .then(user => {
+            if(user && bcrypt.compareSync(password, user.password)) {
+                res.status(200).json({message: `Welcome ${user.username}`})
+            } else {
+                res.status(401).json({message: 'Invalid Username or Password'})
+            }
+        }).catch(err => res.status(500).json({message: 'Server Error', err}
+        ))
+});
 
 
 module.exports = router;
